49708cb6b9fe75b46213d34721a7f41268873c21
risrobs <robertivanov22@abv.bg>
2021-12-17 00:52:03 +0200

    ty

--- src/js/Application.js
+++ src/js/Application.js
@@ -10,12 +10,33 @@ export default class Application extends EventEmitter {
   constructor() {
     super();
     this.emojis = [];
-    this.banana = "🍌";
+    this.banana = "🍌";  
     this.emit(Application.events.READY);
+    this.div = document.getElementById("emojis")
   }
   setEmojis(emojis) {
     this.emojis = emojis;
+
+    this.removeAllChildNodes(this.div)
+    
+    this.emojis.forEach(emoji => {
+      const p = document.createElement("p")
+      p.textContent = emoji;
+      this.div.appendChild(p)
+    })
   }
 
-  addBananas() {}
+  addBananas() {
+   const monkeys = this.emojis.map(element => {
+      return element += this.banana
+    })
+
+    this.setEmojis(monkeys)
+  }
+
+   removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
 }
